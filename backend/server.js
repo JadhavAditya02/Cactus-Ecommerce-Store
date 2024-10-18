704f0249254b23e13eff6f668aab89cb4f88d1f4
Aditya <aditya.jadhav@comp.sce.edu.in>
2024-10-18 14:51:38 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import path from "path";
 import cors from "cors";
 
 
@@ -18,7 +17,6 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" })); 
 app.use(cookieParser()); 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000', // Backend server
+        target: 'https://cactus-store.onrender.com', // Backend server
         changeOrigin: true,
         secure: false,
       },
